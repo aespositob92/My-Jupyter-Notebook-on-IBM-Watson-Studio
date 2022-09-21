a4088ebfc05b67273265cfee08e21581ffcada30
aespositob92 <113705871+aespositob92@users.noreply.github.com>
2022-09-21 16:15:58 -0500

    My Jupyter Notebook on IBM Watson Studio

---
+++ firstpython.py
@@ -0,0 +1,4 @@
+# alessandra Esposito #
+# Analista de Experiencia del Cliente #
+# Estoy interesado en la Data Science porque quiero aplicarlo actualmente en la empresa en la que trabajo #
+# 1 + 1 #
